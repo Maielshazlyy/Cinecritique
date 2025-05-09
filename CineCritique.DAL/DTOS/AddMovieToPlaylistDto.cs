250c399823c79a44d8787e4e4cffe5b135259e2c
Maielshazlyy <119226285+Maielshazlyy@users.noreply.github.com>
2025-05-09 19:50:59 +0300

    Add files via upload

---
+++ CineCritique.DAL/DTOS/AddMovieToPlaylistDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineCritique.DAL.DTOS
+{
+  public  class AddMovieToPlaylistDto
+    {
+        public int PlaylistId { get; set; }
+        public int MovieId { get; set; }
+    }
+}
---
+++ CineCritique.DAL/DTOS/CreatePlaylistDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineCritique.DAL.DTOS
+{
+  public  class CreatePlaylistDto
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+    }
+}
